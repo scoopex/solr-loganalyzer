4632c0b3cafdaaadf844ab627fbb084fcb9ad74e
Marc Schoechlin <marc.schoechlin@vico-research.com>
2018-11-13 08:48:12 +0100

    Refactor for python 3

--- README.md
+++ README.md
@@ -4,63 +4,64 @@ solr-loganalyzer
 A query analyzer that parses Solr's log file to get some basic query statistics 
 
 Note that you need to enable logging at the INFO level for this to
-work and you need to have cores enabled. You also need python2.7 installed.
+work and you need to have cores enabled. You also need python3 installed.
 
 Usage:
+```
+# reading from stdin
+cat file1 file2 | ./loganalyzer.py --max 20
+tail -1000 | ./loganalyzer.py --max
 
-    # reading from stdin
-    head -n <NUMBER OF LINES TO ANALYZE> solrlog| ./loganalyzer.py
-
-    # reading a file
-    head -n <...> > /tmp/outfile.txt
-    ./loganalyzer.py /tmp/outfile.txt
+# reading a file
+./loganalyzer.py so solr.log solr.log.1 solr.log.2
+```
 
 The analyzer outputs statistics grouped by Solr core. Here is an example:
 
-<pre>
+```
 Top Endpoints for core1
 ========================================
-1) "/mlt" 3
+1) "/mlt" 3 times
    
 Top Searh URLs for core1
 ========================================
-1) "mlt.count=16&&q=qrows=16" 1
-2) "mlt.count=16&start=0&q=&rows=16" 1
+1) "mlt.count=16&&q=qrows=16" 1 times
+2) "mlt.count=16&start=0&q=&rows=16" 1 times
    
 Slowest Searches for core1
 ========================================
-1) "mlt.count=16&&q=qrows=16" 30
-2) "mlt.count=16&start=0&q=&rows=16" 11
+1) "mlt.count=16&&q=qrows=16" 30 times
+2) "mlt.count=16&start=0&q=&rows=16" 11 times
    
 Search Time for core1
 ========================================
 Median     2
-75%     11
-90%     11
-99%     30
+75%       11
+90%       11
+99%       30
 
 ****************************************************************************************************
 
 Top Endpoints for core2
 ========================================
-1) "/mlt" 3
+1) "/mlt" 3 times
    
 Top Searh URLs for core2
 ========================================
-1) "mlt.count=5&&q=qrows=16" 1
-2) "mlt.count=5&start=0&q=&rows=16" 1
+1) "mlt.count=5&&q=qrows=16" 1 times
+2) "mlt.count=5&start=0&q=&rows=16" 1 times
    
 Slowest Searches for core2
 ========================================
-1) "mlt.count=5&&q=qrows=16" 30
-2) "mlt.count=5&start=0&q=&rows=16" 11
+1) "mlt.count=5&&q=qrows=16" 30 ms
+2) "mlt.count=5&start=0&q=&rows=16" 11 ms
    
 Search Time for core2
 ========================================
 Median     2
-75%     11
-90%     11
-99%     30
-</pre>
+75%       11
+90%       11
+99%       30
+```
 
 This tool is inspired by [redis-faina](https://github.com/Instagram/redis-faina)
--- loganalyzer.py
+++ loganalyzer.py
@@ -1,25 +1,29 @@
+#!/usr/bin/env python3
+
 from collections import Counter
 import re
 import argparse
 import sys
 
-LINE_RE = re.compile("INFO:\s+\[(?P<core>\w+)\]\s+webapp=/\w+\s+path=(?P<path>/\w+)\s+params={(?P<search>.*)}\s+status=\w+\s+QTime=(?P<qtime>\w+)")
+# INFO  - 2018-11-12 08:42:22.162; org.apache.solr.core.SolrCore; [core1]
+LINE_RE = re.compile("INFO .*?\[(?P<core>\w+)\]\s+webapp=/\w+\s+path=(?P<path>/\w+)\s+params={(?P<search>.*)}\s+(hits=(?P<hits>\w+)\s+)?status=\w+\s+QTime=(?P<qtime>\w+).*")
 """
 lines we want:
-INFO: [arstechnicacogtree] webapp=/solr path=/mlt params={mlt.count=16&fl=link,title,author,pub_date,thumb_url_medium,metadata,section&start=0&q=link_aliases:http\://arstechnica.com/apple/news/2009/02/the\-case\-of\-the\-app\-store\-ripoff.ars&wt=json&fq=pub_date:[NOW/DAY-14DAYS+TO+NOW/DAY%2B1DAY]&rows=16} status=0 QTime=2
+INFO  2018-11-12 08:42:22.162; org.apache.solr.core.SolrCore; [arstechnicacogtree] webapp=/solr path=/mlt params={mlt.count=16&fl=link,title,author,pub_date,thumb_url_medium,metadata,section&start=0&q=link_aliases:http\://arstechnica.com/apple/news/2009/02/the\-case\-of\-the\-app\-store\-ripoff.ars&wt=json&fq=pub_date:[NOW/DAY-14DAYS+TO+NOW/DAY%2B1DAY]&rows=16} status=0 QTime=2
 INFO: [places] webapp=/solr path=/select/ params={pf=*&sort=geodist()+asc&fl=*,_dist_:geodist()&q=*&sfield=location&pt=-1.260326940083812,51.759690475011105&wt=json&spellcheck.collate=true&defType=edismax} hits=9830 status=0 QTime=11
 """
 
 class CoreCounter(object):
-    def __init__(self,corename):
+    def __init__(self, corename):
         self.corename = corename
         self.endpoints = Counter()
         self.urls = Counter()
         self.linesread = 0
         self.qtimes = Counter()
-        
+        self.qtimes_sum = Counter()
+
     def __repr__(self):
-        return "<Core '%s' with %i endpoints %i search urls>" %(self.corename,len(self.endpoints), len(self.urls))
+        return "<Core '%s' with %i endpoints %i search urls>" % (self.corename, len(self.endpoints), len(self.urls))
 
     def timestats(self):
         sqtimes = sorted(self.qtimes.values())
@@ -33,68 +37,100 @@ class CoreCounter(object):
                 ("90%", percent_90),
                 ("99%", percent_99))
 
-    def _pprint_topn(self,n,counter,title):
+    def _pprint_topn(self, n, counter, title, unit):
         """Print the top N entries of a counter with its title.
         """
-        s = "{0}\n{1}\n".format(title, "="*40)
+        s = "{0}\n{1}\n".format(title, "=" * 40)
         top = counter.most_common(n)
         for index, item in enumerate(top):
             label, cnt = item
-            s += '%i) "%s" %i\n' % (index+1, label, cnt)
+            s += 'QUERY %i: "%s" %i %s\n\n' % (index + 1, label, cnt, unit)
         return s
 
     def pprint_stats(self):
         """Print statistics for a core
         """
-        print self._pprint_topn(10, self.endpoints, "Top Endpoints for {0}".format(self.corename))
-
-        print self._pprint_topn(10, self.urls, "Top Search URLs for {0}".format(self.corename))
-
-        print self._pprint_topn(10, self.qtimes, "Slowest Searches for {0}".format(self.corename))
-
+        print(self._pprint_topn(args.max, self.endpoints, "Top Endpoints for {0}".format(self.corename), "times"))
+        print(self._pprint_topn(args.max, self.urls, "Top Search URLs for {0}".format(self.corename), "times"))
+        print(self._pprint_topn(args.max, self.qtimes, "Slowest Searches for {0}".format(self.corename), "ms"))
         # qtimes
-        print "Search Time for {0}\n{1}\n".format(self.corename, '='*40)
+        print("Search Time for {0}\n{1}\n".format(self.corename, '=' * 40))
         stats = self.timestats()
         for st in stats:
-            print "%s     %s" % (st[0], st[1])
+            print("%-10s %sms" % (st[0], st[1]))
         return ""
-    
+
+
 class StatCounter(object):
-    def __init__(self):
+    def __init__(self, debug=False):
         self.corecounters = {}
-        
-    def process(self,iterinput):
+        self.queries = 0
+        self.lines = 0
+        self.debug = debug
+
+    def process(self, iterinput):
         for line in iterinput:
+            self.lines += 1
+
             matches = LINE_RE.match(line)
             if not matches:
+                if self.debug:
+                    print("not matched: >>>{0}<<<".format(line))
                 continue
-            core, path, search, qtime = matches.groups()
 
-            corecounter = self.corecounters.get(core,CoreCounter(core))
+            core = matches.group("core")
+            path = matches.group("path")
+            search = matches.group("search")
+            qtime = matches.group("qtime")
+            hits = matches.group("hits")
+
+
+            corecounter = self.corecounters.get(core, CoreCounter(core))
             corecounter.endpoints[path] += 1
             corecounter.urls[search] += 1
             corecounter.linesread += 1
             corecounter.qtimes[search] = int(qtime)
-            
+            corecounter.qtimes_sum[search] = corecounter.qtimes_sum[search] + int(qtime)
+
             self.corecounters[core] = corecounter
-            
+            self.queries += 1
+
     def allcounterstats(self):
         for cc in self.corecounters.values():
-            print cc.pprint_stats()
-            print "*"* 100        
-            print
+            print(cc.pprint_stats())
+            print("*" * 100)
+            print()
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
+
     parser.add_argument(
-        'input',
-        type = argparse.FileType('r'),
-        default = sys.stdin,
-        nargs = '?',
-        help = "File to parse; will read from stdin otherwise")
-    args = parser.parse_args()
+        '--max',
+        type=int,
+        default=10,
+        nargs='?',
+        help='number of top queries')
+
+    parser.add_argument(
+        '--debug',
+        help='Output debug information',
+        action='store_true',
+    )
+
+    args, remaining_args = parser.parse_known_args()
 
     sc = StatCounter()
-    sc.process(args.input)
+
+    if len(remaining_args) <= 0:
+        sc.process(sys.stdin)
+    else:
+        for filename in remaining_args:
+            print("parsing %s" % filename)
+            with open(filename, "r") as fd:
+                sc.process(fd)
+
     sc.allcounterstats()
+
+    print("parsed %s lines with %s queries" % (sc.lines, sc.queries))
+
